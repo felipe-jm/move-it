e0e81e4f02bda27b77b6b6cba0fa15b2f4854005
Felipe Jung <felipemattoseu@gmail.com>
2021-03-01 11:35:24 -0400

    add login page

---
+++ src/pages/login.tsx
@@ -0,0 +1,30 @@
+import { signIn } from 'next-auth/client';
+
+import styles from '../styles/pages/Login.module.css';
+
+const Login = () => (
+  <div className={styles.container}>
+    <div className={styles.left}>
+      <img
+        src="/background.svg"
+        alt="Parte da Logo da Move it (Três barras semi transparentes)"
+      />
+    </div>
+
+    <div className={styles.right}>
+      <img src="/logos/logo-full-white.svg" alt="Move it Logo" />
+
+      <h1>Bem-vindo</h1>
+
+      <main>
+        <img src="/icons/github.svg" alt="Logo do Github" />
+
+        <p>Faça login com seu Github para começar</p>
+      </main>
+
+      <button onClick={() => signIn()}>Entrar</button>
+    </div>
+  </div>
+);
+
+export default Login;
