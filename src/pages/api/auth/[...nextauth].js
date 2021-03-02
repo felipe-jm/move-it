d89fa39eac3b5139ac0226aef4f742dbc64d4ad7
Felipe Jung <felipemattoseu@gmail.com>
2021-03-02 10:38:25 -0400

    fix: github auth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,11 +1,13 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
-export default NextAuth({
+const options = {
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET
     })
   ]
-});
+};
+
+export default (req, res) => NextAuth(req, res, options);
