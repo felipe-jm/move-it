90d7866476f105c9e755e71216ec21d9943bac95
Felipe Jung <felipemattoseu@gmail.com>
2021-03-04 11:22:44 -0400

    fix word spelling

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -50,7 +50,7 @@ const ChallengeBox = () => {
         </div>
       ) : (
         <div className={styles.challengeNotActive}>
-          <strong>Fianlize um ciclo para receber um desafio</strong>
+          <strong>Finalize um ciclo para receber um desafio</strong>
           <p>
             <img src={`icons/level-up.svg`} alt="Level Up" />
             Avance de level completando desafios.
